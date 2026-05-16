89927c10b0698ee70f61e68f5be6562a835c790a
Nick Harvey <nick@bluemini.com>
2026-05-15 18:57:05 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    AFR = serial.readLine()
+})
+let MAP = 0
+let AFR = ""
+serial.setBaudRate(BaudRate.BaudRate9600)
 basic.forever(function () {
-	
+    MAP = pins.analogReadPin(AnalogPin.P0)
+    datalogger.log(
+    datalogger.createCV("Time", input.runningTime()),
+    datalogger.createCV("AFR", AFR),
+    datalogger.createCV("MAP", MAP)
+    )
+    basic.pause(1000)
 })
