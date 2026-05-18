33cb7491956d873c7c344178826aa8e9fd3882bb
Nick Harvey <nick@bluemini.com>
2026-05-17 22:36:26 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,30 @@
+pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
+    ThisTime = control.millis()
+    list.pop()
+    list[0] = ThisTime
+    TimingSum = 0
+    for (let index = 0; index <= list.length; index++) {
+        TimingSum = TimingSum + list[index]
+    }
+    RPM = TimingSum / list.length
+})
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     AFR = serial.readLine()
 })
 let MAP = 0
 let AFR = ""
-serial.redirect(
-SerialPin.USB_TX,
-SerialPin.P0,
-BaudRate.BaudRate115200
-)
+let RPM = 0
+let TimingSum = 0
+let ThisTime = 0
+let list: number[] = []
+list = [
+0,
+0,
+0,
+0
+]
 basic.forever(function () {
-    MAP = pins.analogReadPin(AnalogReadWritePin.P1)
-    datalogger.mirrorToSerial(true)
-    datalogger.log(
-    datalogger.createCV("AFR", AFR),
-    datalogger.createCV("MAP", MAP)
-    )
-    basic.pause(1000)
+    MAP = pins.i2cReadNumber(24, NumberFormat.Int16LE, false)
+    serial.writeLine("" + RPM + "," + MAP + ",Value")
+    basic.pause(200)
 })
